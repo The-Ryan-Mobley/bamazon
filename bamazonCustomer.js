6bca0b7a16a454a040b6064414ceaacb55317567
Ryan Mobley <ryan.mobley94@gmail.com>
2019-09-06 13:24:49 -0500

    work from 9-5

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -24,6 +24,7 @@ const connection = sql.createConnection({
           console.log(`PRODUCT ID: ${index.id}  PRODUCT NAME: ${index.product_name} PRICE: ${index.price}`);
           productArr.push(index.product_name);
         });
+        console.log(productArr);
         //pass products into pormpt search so you can use the list to purchase items
         promptSearch(productArr);
       });
@@ -33,12 +34,13 @@ const connection = sql.createConnection({
       {
         type:'list',
         message:'what do you want to buy?',
-        choices:arr
+        choices: arr,
+        name:'item'
       }
 
     ]).then((re)=>{
-      let query =re.item.toString();
-      connection.query(`SELECT id, product_name, price FROM products WHERE product_name LIKE '%${query}%'`, (error, results)=> {
+      let DBquery =`%${re.item.toString()}%`;
+      connection.query(`SELECT id, product_name, price FROM products WHERE product_name LIKE ?`,DBquery, (error, results)=> {
         if (error) throw error;
         console.log(results);
         results.forEach((index)=>{
@@ -59,34 +61,38 @@ const connection = sql.createConnection({
     inquirer.prompt([
       {
         type:'confirm',
-        message:'BUY?',
+        message:'do you want to buy this item?',
+        name:'boo'
+      },
+      {
+        type:'number',
+        message:'how many?',
         name:'num'
       }
     ]).then((re)=>{
-      if(re.num === true){
-        
+      if(re.boo === true){
         console.log(reID);
-        Purchase(reID);
+        Purchase(reID,re.num);
         
 
       }
 
     });
   }
-
-  function Purchase(prodID){ //log the varibles out
+ 
+  function Purchase(prodID,numeric){ //log the varibles out
     let itemObj = JSON.parse(prodID);
     //needs to UPDATE database //take value from initial search query and mod it based on that
     console.log(itemObj.id);
-    connection.query(`UPDATE products SET stock_qty = stock_qty - 1 WHERE id =${itemObj.id}`,(err,results)=>{
+    connection.query(`UPDATE products SET stock_qty = stock_qty - ? WHERE id =?`,[numeric,itemObj.id],(err,results)=>{
       if(err) throw err;
-      console.log('oh yeah its all coming together' + results);
+      console.log('oh yeah its all coming together' + JSON.stringify(results));
       connection.end();
 
 
     });
   }
-  f
+
 
 
   welcomeList();
