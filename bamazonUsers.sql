beb58f12c753286002c9f3ef326f86b355d3a46a
Ryan Mobley <ryan.mobley94@gmail.com>
2019-09-11 14:59:52 -0500

    passwords hash properly

--- bamazonUsers.sql
+++ bamazonUsers.sql
@@ -3,7 +3,8 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	id INTEGER(11) AUTO_INCREMENT NOT NULL,
     user_name VARCHAR(30) NOT NULL,
-    pass VARCHAR(30) NOT NULL,
+    pass VARCHAR(1000) NOT NULL,
+    salt VARCHAR(1000) NOT NULL,
     user_type VARCHAR(30),
     
     PRIMARY KEY (id)
--- index.js
+++ index.js
@@ -5,27 +5,72 @@ const connection = require('./db.js');
 //const customer = require('./bamazonCustomer.js');
 //const manager = require('./bamazonManager.js');
 const supervisor = require('./bamazonSupervisor.js');
-const crypto = require('crypto');
-const cryptoSecret =  process.env.CRYPTO_SECRET.toString();
 
-//let s = new supervisor();
-supervisor.main();
-function createLogin(){
+
+//used for password encryption
+const crypto = require('crypto');
+const cryptoSecret = process.env.CRYPTO_SECRET.toString();
+let genRandomString = (length) => { //makes the hash salt
+  return crypto.randomBytes(Math.ceil(length / 2))
+    .toString('hex') /** convert to hexadecimal format */
+    .slice(0, length); /** return required number of characters */
+};
+let sha512 = (password, salt)=>{
+  let hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+  hash.update(password);
+  let value = hash.digest('hex');
+  return {
+      salt:salt,
+      passwordHash:value
+  };
+};
+
+
+function Login(){
   inquirer.prompt([
     {
       type:'input',
-      message:'Enter your desired username under 30 characters: ',
-      name:'userName'
+      message:'Enter Username',
+      name:'user'
+    },
+    {
+      type:'input',
+      message:'Enter Password',
+      name:'pass'
     }
   ]).then((re)=>{
-    if(re.userName.length <= 30){
-      connection.query('SELECT user_name FROM users',(er,re)=>{
-        if(er) throw er;
-        if(re.indexOf(re.userName) === -1){
+    connection.query(`SELECT pass,salt FROM users WHERE user_name = ?`,re.user,(er,base)=>{
+      if(er) throw er;
+      console.log(base);
+      let hashPass = readHash(re.pass,base[0].salt);
+      if(base[0].pass === hashPass){
+        console.log('validated');
+      }
+      else{
+        console.log('invalid');
+      }
+
+    });
+  })
+}
+//let s = new supervisor();
+//supervisor.main();
+function createLogin() {
+  inquirer.prompt([{
+    type: 'input',
+    message: 'Enter your desired username under 30 characters: ',
+    name: 'userName'
+  }]).then((re) => {
+ 
+    if (re.userName.length <= 30) {
+      connection.query('SELECT user_name FROM users', (er, queryResponse) => {
+        if (er) throw er;
+        
+        if (queryResponse.indexOf(re.userName) === -1) {
+       
           createPassword(re.userName);
 
-        }
-        else{
+        } else {
           console.log('username already taken please enter a different name');
           createLogin();
         }
@@ -33,27 +78,40 @@ function createLogin(){
 
     }
 
-    
+
 
   })
 
 }
-function createPassword(userName){
-  inquirer.prompt([
-    {
-      type:'input',
-      message:'enter your desired password: ',
-      name:'passWord'
-    }
-  ]).then((re)=>{
+
+function createPassword(userName) {
+  inquirer.prompt([{
+    type: 'input',
+    message: 'enter your desired password: ',
+    name: 'passWord'
+  }]).then((re) => {
+    hashIt(re.passWord,userName);
 
   });
 }
-function hashIt(string){
-  let salt = crypto.randomBytes(16).toString('hex');
-  const hash = crypto.createHmac(string, cryptoSecret)
-                   .update('I love cupcakes')
-                   .digest('hex');
 
+function hashIt(string,userName) {
+    let salt = genRandomString(16); /** Gives us salt of length 16 */
+    let passwordData = sha512(string, salt);
+    console.log('UserPassword = '+string);
+    console.log('Passwordhash = '+passwordData.passwordHash);
+    console.log('nSalt = '+passwordData.salt);
+    let sqlString = `INSERT INTO users (user_name,pass,salt) VALUES (?,?,?)`;
+    
+    connection.query(sqlString,[userName,passwordData.passwordHash,passwordData.salt],(er)=>{
+      if(er) throw er;
+      console.log('user added!');
+    });
+}
+function readHash(string,salt){ 
+  let passwordData = sha512(string, salt);
+  return passwordData.passwordHash;
 
 }
+ 
+Login();
